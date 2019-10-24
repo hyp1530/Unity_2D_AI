24fa8870a7ce05c46e9af9d679eff039b309c264
unknown <hyp1530@gmail.com>
2019-10-24 12:10:05 +0800

    1024角色左右控制

--- 2D/Assets/Fox.cs
+++ 2D/Assets/Fox.cs
@@ -2,10 +2,27 @@
 
 public class Fox : MonoBehaviour
 {
+    public int speed = 50;
+    public float jump = 2.5f;
+    public string foxName = "狐狸";
+    public bool pass = false;
 
+    private Rigidbody2D r2D;
+   // private Transform tra;
     private void Start()
     {
+        r2D = GetComponent<Rigidbody2D>();
+    //    tra = GetComponent<Transform>();
         
     }
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.D)) transform.eulerAngles = new Vector3(0, 0, 0);
+        if (Input.GetKeyDown(KeyCode.A)) transform.eulerAngles = new Vector3(0, 180, 0);
+    }
+    private void FixedUpdate()
 
+    {        
+        r2D.AddForce(new Vector2(speed*Input.GetAxis("Horizontal"), 0));
+    }
 }
