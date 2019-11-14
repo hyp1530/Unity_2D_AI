3a0048b3988945f9b0d17f8e9dd9e36280731627
hyp1530 <55218857+hyp1530@users.noreply.github.com>
2019-11-14 12:06:40 +0800

    1114

---
+++ 2D/Assets/LearnSwitch.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LearnSwitch : MonoBehaviour
+{
+    public string myColor = "黑色";
+
+    public enum season
+    {
+        spring, summer, fall, winter
+    }
+    public season _season = season.fall;
+
+    private void Start()
+    {
+        if (myColor == "黑色")
+        {
+            print("玩家輸入的是黑色");
+        }
+        else if (myColor == "白色")
+        {
+            print("玩家輸入的是白色");
+        }
+
+    }
+    
+}
--- 2D/Assets/NPC.cs
+++ 2D/Assets/NPC.cs
@@ -1,8 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class NPC : MonoBehaviour {
+    public enum state
+    {
+        normal,notComplete,complete
+    }
+    public state _state;
+
     [Header("對話")]
     public string SayStart = "嗨!你好，幫我找到十個果實。";
     public string SayNotComplete = "你還沒找到十個果實。";
@@ -13,13 +20,34 @@ public class NPC : MonoBehaviour {
     public bool mission = false;
     public int CountPlayer = 0;
     public int CountFinish = 10;
-    // Use this for initialization
-    void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    [Header("介面")]
+    public GameObject objCanvas;
+    public Text textSay;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.name == "fox")
+        {
+            Say();
+
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.name == "fox")
+        {
+            SayClose();
+            
+        }
+
+    }
+    private void Say()
+    {
+        objCanvas.SetActive(true);
+        textSay.text = SayStart;
+    }
+    private void SayClose()
+    {
+
+        objCanvas.SetActive(false);
+    }
 }
